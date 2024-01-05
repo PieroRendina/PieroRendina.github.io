9d1daaf1bd2e5584b7a8e930aefa773978a8d21a
Piero Rendina <piero.rendina@mail.polimi.it>
2024-01-05 16:19:38 +0100

    added entry to projects page

---
+++ script.js
@@ -0,0 +1,6 @@
+function addProjectDescription() {
+    let descriptionParagraph = document.querySelector("#project-description");
+    descriptionParagraph.innerHTML = "IndyCS was a project in collaboration with the Polimi Graduate School of management";
+}
+
+addProjectDescription();
